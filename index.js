4047b36da4e270e4a55b193a3e044af17f1420c3
Nicole Steadman <nsteadman@gmail.com>
2021-11-18 19:22:48 -0800

    commit tree 495505ea8e9317eb3a5d9734d65d1a8cc6155005

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let reverseInput = word.split('').reverse().join('');
+  if (word === reverseInput) {
+    return true;
+  } else {
+    return false;
+  }
+  
 }
 
 /* 
   Add your pseudocode here
+  define a function called isPalindrome with a string as an argument
+    let string equals string lowercase
+    let new string equal string with split to isolate letters of word
+    use reverse to reverse letters of word
+    use join to join the letters back into a word
+    if lowercase string equals reverse string
+      return true
+    
+    else return false
 */
 
 /*
   Add written explanation of your solution here
+  the first step is to save the lowercase version of the input as a variable.
+  then split up the letters, reverse the order of the letters, and join them back together.
+  compare the first input variable with the reverse variable as a boolean.
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +39,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("Hello"));
 }
 
 module.exports = isPalindrome;
